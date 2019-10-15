239b8470f332496845ae0361f428890810ba478f
Unknown <152804004@ogr.cbu.edu.tr>
2019-10-16 01:52:24 +0300

    Add Mongoose and Create DB, Add İtem
    -

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+
+//mongoose.Promise = global.Promise;
+
+/////Mongoose Connetion
+mongoose.connect("mongodb://localhost/todo",{
+    
+}).then(function(){
+    console.log("Database Conneted");
+})
 
 
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-var todoList = [
-    "Wash the car change oil",
-    "Clean Room"
-]
+var todoSchema = mongoose.Schema({
+    name: String
+});
+
+var Todo = mongoose.model("Todo",todoSchema);
+
+//var todoList = [
+//    "Wash the car change oil",
+//    "Clean Room"
+//]
 
 //==========Express Routes===========//
 
 
 //Default Route
 app.get("/",function(req,res){
-    res.render("index.ejs",{todoList:todoList});
+    Todo.find({},function(err,todoList){
+        if(err) console.log(err);
+        else{
+            res.render("index.ejs",{todoList:todoList});
+        }
+    })
+   
 });
 
 //Submit Button Route
 app.post("/newtodo",function(req,res){
     console.log("item submitted !");
-    var item = req.body.item;
-    todoList.push(item);
+    var newItem = new Todo({
+        name : req.body.item
+    });
+    Todo.create(newItem,function(err,Todo){
+        if(err) console.log(err);
+        else{
+            console.log("Inserted Item : "+newItem);
+        }
+    });
+    //todoList.push(item);
     res.redirect("/");
 });
 
